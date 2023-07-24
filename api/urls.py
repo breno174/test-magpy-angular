0031e804d89ed8470075524fc11fbde30a68d2cd
brenoblu <breno.angelim@hotmail.com>
2023-07-24 07:34:24 -0300

    feat: begin test

---
+++ api/urls.py
@@ -0,0 +1,31 @@
+## from django.conf.urls import url
+from rest_framework import routers
+from api import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
+from django.urls import re_path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="MagPy API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny),
+)
+
+router = DefaultRouter()
+
+router.register(r"projects", views.ProjectViewSet, basename="projects")
+
+urlpatterns = [
+    re_path(
+        r"^swagger$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+]
+
+urlpatterns += router.urls
---
+++ api/views.py
@@ -0,0 +1,19 @@
+from django.http.response import Http404
+from rest_framework.response import Response
+from api.permissions import ProjectPermission
+from rest_framework import generics, status, viewsets
+
+from .models import Project, PackageRelease
+from .serializers import ProjectRetriveSerializer, ProjectSerializer, PackageSerializer
+from rest_framework.decorators import action
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    permission_classes = [ProjectPermission]
+    serializer_class = ProjectSerializer
+    lookup_field = "name"
+
+    def get_queryset(self):
+        queryset = Project.objects.all()
+
+        return queryset
